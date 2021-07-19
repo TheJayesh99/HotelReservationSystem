18f51c479728cdc0233ef0c6e02cddc49b82532f
TheJayesh99 <jayeshm.official@gmail.com>
2021-07-19 08:34:36 +0530

    Added : welcome message

---
+++ lib/src/main/java/com/bridgelabz/hotelreservationsystem/HotelReservationSystemMain.java
@@ -0,0 +1,9 @@
+package com.bridgelabz.hotelreservationsystem;
+
+public class HotelReservationSystemMain
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("Welcome to Hotel Reservation System");
+	}
+}
