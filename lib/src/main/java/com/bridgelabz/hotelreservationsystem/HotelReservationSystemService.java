b0f04768b694aa7918f5dde5b7b2939eec56c9d8
TheJayesh99 <jayeshm.official@gmail.com>
2021-07-19 19:14:25 +0530

    Added : Method to find cheapest hotel for given date range

--- lib/src/main/java/com/bridgelabz/hotelreservationsystem/HotelReservationSystemService.java
+++ lib/src/main/java/com/bridgelabz/hotelreservationsystem/HotelReservationSystemService.java
@@ -1,6 +1,7 @@
 package com.bridgelabz.hotelreservationsystem;
-
+import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public class HotelReservationSystemService 
@@ -11,5 +12,40 @@ public class HotelReservationSystemService
 	public void addHotel(Hotel hotel) 
 	{
 		hotelsList.add(hotel);
+	}
+	
+	public String FindCheapestHotel(String startDate,String endDate) 
+	{
+		LocalDate localStartDate = LocalDate.parse(startDate); 
+		LocalDate localEndDate = LocalDate.parse(endDate);
+		
+		if (localStartDate.compareTo(localEndDate) <= 0 && localStartDate.compareTo(LocalDate.now()) >= 0)
+		{
+			HashMap<String,Integer> hotelPricesList = new HashMap<>(); //making a price list to have mapping with price and hotels
+			while(localStartDate.compareTo(localEndDate) <= 0)
+			{
+				for (Hotel hotel : hotelsList) 
+				{		
+					if (hotelPricesList.containsKey(hotel.getHotelName()))   //if hotel already exists then increment the hotel price
+					{
+						int exsistingAmount =  hotelPricesList.get(hotel.getHotelName());
+						exsistingAmount += hotel.getWeekDaysRateForRegular(); 
+						hotelPricesList.put(hotel.getHotelName(), exsistingAmount);													
+					}
+					else
+					{							
+						hotelPricesList.put(hotel.getHotelName(), hotel.getWeekDaysRateForRegular());  //adding new hotel to the hotel price list if hotel not in list						
+					}
+				}
+				localStartDate=localStartDate.plusDays(1); //incrementing the days
+			}
+			
+			//finding the hotel with minimum price
+			return  hotelPricesList.entrySet().stream().min((entry1,entry2) -> entry1.getValue().compareTo(entry2.getValue())).get().getKey(); 
+		}
+		else
+		{
+			return "Invalid dates" ;
+		}
 	}
 }
